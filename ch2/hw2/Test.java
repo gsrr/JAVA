a2da88809ee6b5cae100bcf390913bbe4603c9b8
gsrr <jerrycheng1128@gmail.com>
2015-07-03 20:33:26 +0800

    update

--- ch2/hw2/Test.java
+++ ch2/hw2/Test.java
@@ -1,12 +1,11 @@
-public class Test
+pblic class Test
 {
         public static void main(String args[])
         {
                 Deck d = new Deck();
                 for(int i = 0 ; i < 52 ; i++)
                 {
-                        Card c = d.getCard();
-                        System.out.println(c.toString());
+                        //...
                 }
 
         }
